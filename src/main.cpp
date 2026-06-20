b08fc542be836280ade36babe43649da84c1a676
Aidan <aidanjcampbell1@gmail.com>
2026-06-20 10:16:25 -0400

    Attempted fix to serial monitor inut problem using millis() but failed

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,30 @@
 #include <Arduino.h>
 
-int waitTime = 1000;
+unsigned long waitTime = 1000; //what does this do?
+unsigned long lastUpdateTime = 0;
+
 int rotation = 0;
 int limit = 10;
 
 void printStatus() {
   Serial.println(rotation);
 
-if (rotation >= limit){
+  if (rotation >= limit){
   Serial.println("WARNING: limit reached! Enter 'R' to reset");
+  }else {
+    Serial.println("OK - Within Range");
+  }
+}
+
+void checkForReset() {
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
-    if (input == "R" or input == "r") {
+    if (input == "R" or input == "r") { 
       Serial.println("Resetting...");
-      delay(3000);
       rotation = 0;
       Serial.println("Reset Complete.");
     }
   }
-}else{
-  Serial.println("OK - Within Range");
-}
 }
 void setup() {
   // put your setup code here, to run once:
@@ -29,7 +33,11 @@ Serial.begin(115200);
 
 void loop() {
   // put your main code here, to run repeatedly:
-  rotation = rotation + 1;
-  printStatus();
-  delay(waitTime);
+  checkForReset();
+  unsigned long currentTime = millis();
+  if (currentTime - lastUpdateTime >= waitTime) {
+    lastUpdateTime = currentTime;
+    rotation = rotation + 1;
+    printStatus();
+  }
 }
